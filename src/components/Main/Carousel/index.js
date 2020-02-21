ace77249b0a2a1704fa63ef5be8863832a4e5132
TysonVonnBeck <54685544+tysonvonnbeck@users.noreply.github.com>
2020-02-21 10:10:35 -0800

    incomming changes

--- src/components/Main/Carousel/index.js
+++ src/components/Main/Carousel/index.js
@@ -22,66 +22,6 @@ const styles = theme => ({
     title: {
         fontSize: 14,
     },
-<<<<<<< HEAD
-}));
-
-function SpacingGrid(props) {
-    const [spacing, setSpacing] = React.useState(2);
-    const classes = useStyles();
-
-    const handleChange = event => {
-        setSpacing(Number(event.target.value));
-    };
-
-let incomingInfo = [
-    {
-    id:0,
-    description: props.description[0],
-    activityName: props.activityName[0]
-    },
-    {
-    id:1,
-    description: props.description[1],
-    activityName: props.activityName[1]
-    },
-    {
-    id:2,
-    description: props.description[2],
-    activityName: props.activityName[2]
-    },
-    {
-    id:3,
-    description: props.description[3],
-    activityName: props.activityName[3]
-    }
-
-];
-
-    return (
-        <Container id="wcon">
-            <Grid container className={classes.root} spacing={3}>
-                <Grid item xs={12}>
-                    <Grid container justify="center" spacing={spacing}>
-                        {incomingInfo.map(value => ( // this one line has been stumping greg. Make this only display 4 results.
-                            <Grid key={value} item>
-                                <Paper className={classes.paper} />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Grid>
-                <Grid item xs={12}>
-                </Grid>
-            </Grid>
-            <Button variant="outlined" disabled>
-                Next
-            </Button>
-        </Container>
-    );
-}
-
-
-export default SpacingGrid()
-=======
     pos: {
         marginBottom: 12,
     },
@@ -133,4 +73,3 @@ class Carousel extends Component {
 
 }
 export default withStyles(styles)(Carousel);
->>>>>>> origin
